1743a4b0ae132b1e270d1c8fcfbc856d78f5d675
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-01-15 15:52:45 +0200

    added colors, size, tools

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
 var context = document.querySelector('#coloring_canvas').getContext('2d');
 
+// 
 var clickX = [];
 var clickY = [];
 var clickDrag = [];
 var paint = false;
+
+// colors
+var colorBlack = '#34495e';
+var colorRed = '#e74c3c';
+var currentColor = colorBlack;
+var clickColor = [];
+
+// size
+var clickSize = [];
+var currentSize = 'normal';
+
+// tools
+var clickTool = [];
+var currentTool = 'marker';
+
 
 // update cursor data
 function addClick(x, y, dragging) {
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
+  clickSize.push(currentSize);
+
+  if (currentTool == 'eraser') {
+    clickColor.push('#ffffff');
+  } else {
+    clickColor.push(currentColor);
+  }
 }
 
 // drawing
 function redraw() {
   clearColoring();
 
-  context.strokeStyle = '#df4b26';
+  var radius;
   context.lineJoin = 'round';
-  context.lineWidth = 5;
 
   for (var i = 0; i < clickX.length; i++) {
+
+    if(clickSize[i] == "small"){
+			radius = 2;
+		} else if(clickSize[i] == "normal"){
+			radius = 5;
+		} else if(clickSize[i] == "large"){
+			radius = 10;
+		} else if(clickSize[i] == "huge"){
+			radius = 20;
+		}
+
     context.beginPath();
     if (clickDrag[i] && i) {
       context.moveTo(clickX[i-1], clickY[i-1]);
@@ -30,6 +63,8 @@ function redraw() {
     context.lineTo(clickX[i], clickY[i]);
     context.closePath();
     context.stroke();
+    context.strokeStyle = clickColor[i];
+    context.lineWidth = radius;
   }
 }
 
@@ -99,6 +134,52 @@ $('#clear_coloring').on('click', function() {
   clickX = [];
   clickY = [];
   clickDrag = [];
+  clickColor = [];
+  clickSize = [];
   clearColoring();
 });
 
+
+/**
+ *  Colors
+ */
+$('#color_black').on('click', function() {
+  currentColor = colorBlack;
+});
+
+$('#color_red').on('click', function() {
+  currentColor = colorRed;
+});
+
+
+/**
+ *  Size
+ */
+$('#size_small').on('click', function() {
+  currentSize = 'small';
+});
+$('#size_normal').on('click', function() {
+  currentSize = 'normal';
+});
+$('#size_large').on('click', function() {
+  currentSize = 'large';
+});
+$('#size_huge').on('click', function() {
+  currentSize = 'huge';
+});
+
+
+/**
+ *  Tools
+ */
+// $('#tool_crayon').on('click', function() {
+//   currentTool = 'crayon';
+// });
+
+$('#tool_marker').on('click', function() {
+  currentTool = 'marker';
+});
+
+$('#tool_eraser').on('click', function() {
+  currentTool = 'eraser';
+});
